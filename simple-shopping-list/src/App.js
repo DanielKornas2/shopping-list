4d36e9ffcef1adce16a39776d8fa61838357639d
Daniel Kornas <harvi_pl@wp.pl>
2018-12-30 14:30:33 +0100

    Initial state

--- simple-shopping-list/src/App.js
+++ simple-shopping-list/src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    items: [
+      { id: 1, name: "produkt1", active: true },
+      { id: 2, name: "produkt2", active: true },
+      { id: 3, name: "produkt3", active: false },
+      { id: 4, name: "produkt4", active: true },
+      { id: 5, name: "produkt5", active: false },
+    ]
+  }
   render() {
     return (
       <div className="App">

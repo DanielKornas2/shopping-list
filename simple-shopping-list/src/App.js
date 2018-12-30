f0b16ba93e83eab3c39e35f8d7d9be9466ec629b
Daniel Kornas <harvi_pl@wp.pl>
2018-12-30 14:47:48 +0100

    Styles and components

--- simple-shopping-list/src/App.js
+++ simple-shopping-list/src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Header = (props) => {
+  return (
+    <header>Podsumowanie zamówienia</header>
+  )
+}
+
+const ListItems = (props) => {
+  return (
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  )
+}
+
 class App extends Component {
   state = {
     items: [
@@ -14,22 +32,10 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Test
-          </a>
-        </header>
-      </div>
+      <>
+        <Header items={this.state.items} />
+        <ListItems />
+      </>
     );
   }
 }
